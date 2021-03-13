a31c11bb71b96d7b389951ef92238288f217483a
renanmoura6941 <naneraruom@gmail.com>
2021-03-13 17:16:16 -0300

    service implements

--- src/main/java/com/spring/todo/todolistspringboot/service/TodoServiceImpl.java
+++ src/main/java/com/spring/todo/todolistspringboot/service/TodoServiceImpl.java
@@ -1,5 +1,41 @@
 package com.spring.todo.todolistspringboot.service;
 
-public class TodoServiceImpl {
+import java.util.List;
+
+import com.spring.todo.todolistspringboot.dto.TodoDTO;
+import com.spring.todo.todolistspringboot.interfaces.ITodoService;
+import com.spring.todo.todolistspringboot.model.Todo;
+
+public class TodoServiceImpl implements ITodoService {
+
+    @Override
+    public List<Todo> getAllTodos() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Todo getTodoById(String id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String createTodo(TodoDTO todo) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Todo updateTodo(String id, TodoDTO todo) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void deleteTodoById(String id) {
+        // TODO Auto-generated method stub
+
+    }
 
 }

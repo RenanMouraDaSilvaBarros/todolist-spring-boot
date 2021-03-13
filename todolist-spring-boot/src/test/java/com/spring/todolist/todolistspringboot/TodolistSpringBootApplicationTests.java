1d3b91aad0bf866dfa121e89b96cc607a98b7b2a
isaiassantossilva <isaias661248@gmail.com>
2021-03-13 15:55:41 -0300

    project files

---
+++ todolist-spring-boot/src/test/java/com/spring/todolist/todolistspringboot/TodolistSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.todolist.todolistspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

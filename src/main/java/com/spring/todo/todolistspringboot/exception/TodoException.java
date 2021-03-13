269bae792ef0cd5d6c7d458828d21bc49b818ed8
renanmoura6941 <naneraruom@gmail.com>
2021-03-13 16:55:09 -0300

    excption

---
+++ src/main/java/com/spring/todo/todolistspringboot/exception/TodoException.java
@@ -0,0 +1,14 @@
+package com.spring.todo.todolistspringboot.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+class TodoException extends ResponseStatusException {
+
+    private static final long serialVersionUID = 1L;
+
+    public TodoException(HttpStatus status, String menssage) {
+        super(status, menssage);
+    }
+
+}
